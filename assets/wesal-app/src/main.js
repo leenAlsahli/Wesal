939368c3a3477aef7b75aa73012b586bb122d0f5
Leen <leenfahadals@gmail.com>
2026-09-24 08:19:17 +0300

    Delete assets directory

--- assets/wesal-app/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router' // استيراد الراوتر
-
-const app = createApp(App)
-app.use(router) // تفعيل الراوتر
-app.mount('#app')
