4cf55fc9f3f7e35347655653efc19d2efc52e0a7
Leen <leenfahadals@gmail.com>
2026-09-24 08:16:24 +0300

    Delete wesal-app directory

--- wesal-app/src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://pdgkdbzdfklwckkfyoly.supabase.co'
-const supabaseKey = 'sb_publishable_VioXmD5M7yo-HtRBlchBDg_Rc4v4Wbc'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
